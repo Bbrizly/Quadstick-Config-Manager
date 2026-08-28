232f26f41cd064e9b17ad44a0e44d7f266901594
Bassam <87358177+Bbrizly@users.noreply.github.com>
2026-08-28 16:39:11 -0300

    fix: import only migrated application namespaces

--- src/QuadStick.App/ArchitectureUsings.cs
+++ src/QuadStick.App/ArchitectureUsings.cs
@@ -1,7 +1,3 @@
-global using QuadStick.Application.Backup;
-global using QuadStick.Application.Community;
 global using QuadStick.Application.Devices;
 global using QuadStick.Application.Profiles;
 global using QuadStick.Application.Settings;
-global using QuadStick.Application.Telemetry;
-global using QuadStick.Application.Updates;
