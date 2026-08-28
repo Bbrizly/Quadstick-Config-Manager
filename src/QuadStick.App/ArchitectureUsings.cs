63953a758ba9f572a3185eee7661cd63ea4e7370
Bassam <87358177+Bbrizly@users.noreply.github.com>
2026-08-28 19:08:50 -0300

    wire application community and update namespaces

--- src/QuadStick.App/ArchitectureUsings.cs
+++ src/QuadStick.App/ArchitectureUsings.cs
@@ -1,8 +1,10 @@
 global using QuadStick.Application.Backup;
+global using QuadStick.Application.Community;
 global using QuadStick.Application.Devices;
 global using QuadStick.Application.Profiles;
 global using QuadStick.Application.Settings;
 global using QuadStick.Application.Telemetry;
+global using QuadStick.Application.Updates;
 global using QuadStick.Infrastructure.Google;
 global using QuadStick.Infrastructure.Security;
 global using QuadStick.Infrastructure.Settings;
