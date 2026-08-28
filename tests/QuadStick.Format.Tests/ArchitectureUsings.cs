9a45d499d4579987faf616c575e0157ac99960dd
Bassam <87358177+Bbrizly@users.noreply.github.com>
2026-08-28 16:43:50 -0300

    test: import finalized backup and google namespaces

--- tests/QuadStick.Format.Tests/ArchitectureUsings.cs
+++ tests/QuadStick.Format.Tests/ArchitectureUsings.cs
@@ -1,4 +1,6 @@
+global using QuadStick.Application.Backup;
 global using QuadStick.Application.Devices;
 global using QuadStick.Application.Profiles;
 global using QuadStick.Application.Settings;
+global using QuadStick.Infrastructure.Google;
 global using QuadStick.Infrastructure.Settings;
