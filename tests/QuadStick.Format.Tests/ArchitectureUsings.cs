2f138d921d95399c049657177cad3cd6da09f897
Bassam <87358177+Bbrizly@users.noreply.github.com>
2026-08-28 16:40:21 -0300

    test: import infrastructure settings compatibility facade

--- tests/QuadStick.Format.Tests/ArchitectureUsings.cs
+++ tests/QuadStick.Format.Tests/ArchitectureUsings.cs
@@ -1,3 +1,4 @@
 global using QuadStick.Application.Devices;
 global using QuadStick.Application.Profiles;
 global using QuadStick.Application.Settings;
+global using QuadStick.Infrastructure.Settings;
