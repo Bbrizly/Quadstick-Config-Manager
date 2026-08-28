7f6cdba6734a831f74d939c358a608695ffcebf3
Bassam <87358177+Bbrizly@users.noreply.github.com>
2026-08-28 19:03:04 -0300

    update architecture test telemetry namespaces

--- tests/QuadStick.Format.Tests/ArchitectureUsings.cs
+++ tests/QuadStick.Format.Tests/ArchitectureUsings.cs
@@ -2,5 +2,7 @@ global using QuadStick.Application.Backup;
 global using QuadStick.Application.Devices;
 global using QuadStick.Application.Profiles;
 global using QuadStick.Application.Settings;
+global using QuadStick.Application.Telemetry;
 global using QuadStick.Infrastructure.Google;
 global using QuadStick.Infrastructure.Settings;
+global using QuadStick.Infrastructure.Telemetry;
