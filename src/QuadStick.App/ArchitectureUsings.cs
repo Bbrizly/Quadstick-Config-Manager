4ddf4371099679e47a805332ccdae157efdee6e7
Bassam <87358177+Bbrizly@users.noreply.github.com>
2026-08-28 16:38:57 -0300

    refactor: import application architecture namespaces into presentation

---
+++ src/QuadStick.App/ArchitectureUsings.cs
@@ -0,0 +1,7 @@
+global using QuadStick.Application.Backup;
+global using QuadStick.Application.Community;
+global using QuadStick.Application.Devices;
+global using QuadStick.Application.Profiles;
+global using QuadStick.Application.Settings;
+global using QuadStick.Application.Telemetry;
+global using QuadStick.Application.Updates;
