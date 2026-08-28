c0fbfed0aec03457ecb58b1903afe13cd14738ad
Bassam <87358177+Bbrizly@users.noreply.github.com>
2026-08-28 19:20:24 -0300

    wire process bridge namespace in app tests

--- tests/QuadStick.App.Tests/ArchitectureUsings.cs
+++ tests/QuadStick.App.Tests/ArchitectureUsings.cs
@@ -2,5 +2,6 @@ global using QuadStick.Application.Devices;
 global using QuadStick.Application.Profiles;
 global using QuadStick.Application.Settings;
 global using QuadStick.Application.Telemetry;
+global using QuadStick.Infrastructure.Agent;
 global using QuadStick.Infrastructure.Settings;
 global using QuadStick.Infrastructure.Telemetry;
