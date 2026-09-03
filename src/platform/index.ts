a635ab430937db080594ac318ec85a2871cda52f
Bassam <87358177+Bbrizly@users.noreply.github.com>
2026-09-03 17:34:50 -0300

    feat(rewrite): export Google auth status contract

--- src/platform/index.ts
+++ src/platform/index.ts
@@ -7,57 +7,21 @@
 
 export type { CommunityCatalog, CommunityClient, CommunityProfile } from "./communityContracts";
 export type {
-  AppSettings,
-  AppSnapshot,
-  Capabilities,
-  CloseDisposition,
-  CloseOutcome,
-  ConfirmationKind,
-  ConfirmationRequirement,
-  DeletePlan,
-  DeleteReceipt,
-  DeviceInvalidation,
-  DeviceLibrarySnapshot,
-  DevicePresenceSnapshot,
-  DeviceProfileEntry,
-  DeviceSummary,
-  EditorOp,
-  EditorSnapshot,
-  InstallPlan,
-  InstallProgress,
-  InstallReceipt,
-  InterfaceScale,
-  Issue,
-  IssueSeverity,
-  LedColour,
-  LiveMotion,
-  LiveSnapshot,
-  LiveStatus,
-  Mode,
-  ModelChoice,
-  PickerGrouping,
-  ProfileSource,
-  QcmErrorPayload,
-  RecoveryAction,
-  SaveReceipt,
-  SettingsPatch,
-  Subscription,
-  ThemeChoice,
+  AppSettings, AppSnapshot, Capabilities, CloseDisposition, CloseOutcome, ConfirmationKind,
+  ConfirmationRequirement, DeletePlan, DeleteReceipt, DeviceInvalidation, DeviceLibrarySnapshot,
+  DevicePresenceSnapshot, DeviceProfileEntry, DeviceSummary, EditorOp, EditorSnapshot, InstallPlan,
+  InstallProgress, InstallReceipt, InterfaceScale, Issue, IssueSeverity, LedColour, LiveMotion,
+  LiveSnapshot, LiveStatus, Mode, ModelChoice, PickerGrouping, ProfileSource, QcmErrorPayload,
+  RecoveryAction, SaveReceipt, SettingsPatch, Subscription, ThemeChoice,
 } from "./contracts";
 export type { RenameDeviceProfileReceipt } from "./deviceRenameContracts";
+export type { GoogleAuthStatus } from "./googleContracts";
 export type {
-  PreferenceCatalog,
-  PreferenceDefinition,
-  PreferenceEditorKind,
-  PreferenceOption,
+  PreferenceCatalog, PreferenceDefinition, PreferenceEditorKind, PreferenceOption,
 } from "./preferenceContracts";
 export type {
-  WorkbookExportReceipt,
-  WorkbookImportReview,
-  WorkbookLimitation,
-  WorkbookMode,
-  WorkbookSkippedTab,
-  WorkbookTabRename,
+  WorkbookExportReceipt, WorkbookImportReview, WorkbookLimitation, WorkbookMode,
+  WorkbookSkippedTab, WorkbookTabRename,
 } from "./workbookContracts";
 export { ERROR_CODES, INTERFACE_SCALES } from "./contracts";
 export { MockQcmClient } from "./mockQcmClient";
